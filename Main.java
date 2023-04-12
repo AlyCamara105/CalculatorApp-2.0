bafe00f81bd68c4a8666404d0107cb4bd02d3b40
Aly <alcamara237@gmail.com>
2023-04-11 21:31:12 -0400

    Created the basic format for main.

---
+++ Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        final Scanner scan = new Scanner(System.in);
+        String input;
+
+        System.out.println("Instructions:\nInput: \"stop\" to exit the calculator, \"clear\" to clear all previous inputs, or \"history\" to show previous calculations.\nUse the following operators or functions to use the calculator: *, /, +, -, ^, %, log, sin, arcsin, cos, arcos, tan, arctan, abs, !.\nFor exponents the first number is the exponent base and the second number is the exponent.\nFor logarithms the first number is the number inputed in the logarithm and the second number is the logarithm base.\nUse \"pi\" for PI and \"e\" for the mathematical constant.");
+        System.out.println("\nEnter a function or operation");
+
+        input = scan.nextLine().toLowerCase();
+    }
+}
